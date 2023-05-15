878f9d7ffba60622df968a75ce1c806b2bc511be
Ahmad Riad Al-adaileh <128195215+ahmadriad@users.noreply.github.com>
2023-05-15 19:34:58 +0400

    Add files via upload

---
+++ Happy Customers/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict if a customer is happy or not based on the answers they give to questions asked.',
+    author='Ahmad',
+    license='',
+)
